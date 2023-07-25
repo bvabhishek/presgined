93df65361b5fb192f7461c18d7e6fbce6dd37ca3
Abhishek bv <61512598+bvabhishek@users.noreply.github.com>
2023-07-25 19:23:21 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,36 @@
+variable "aws_region" {
+  default = "us-west-2"
+}
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "random_string" "random_name" {
+  length  = 10
+  special = false
+  upper   = false
+}
+
+resource "aws_s3_bucket" "example_bucket" {
+  bucket = "presigned-${random_string.random_name.result}"
+}
+
+resource "aws_s3_bucket_object" "example_object" {
+  bucket = aws_s3_bucket.example_bucket.bucket
+  key    = "seasides.jpeg"
+  acl    = "private"
+  source = "seasides.jpeg"
+}
+
+output "s3bucketname" {
+  value = {
+    BucketName = aws_s3_bucket.example_bucket.bucket
+    URL = format("https://s3.console.aws.amazon.com/s3/buckets/%s/?region=%s",aws_s3_bucket.example_bucket.bucket,var.aws_region)
+    arn        = aws_s3_bucket.example_bucket.arn
+    ObjectURL = aws_s3_bucket_object.example_object.website_endpoint
+  }
+}
+
+https://presigned-pfv8y047ul.s3.us-west-2.amazonaws.com/seasides.jpeg
+
+https://s3bucketname.s3.us-west-2.amazonaws.com/seasides.jpeg
